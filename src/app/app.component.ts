f46610c7bb6f859cc4a68223476f51dc3dd524dc
kevinedlyn <c14180208@john.petra.ac.id>
2021-02-23 19:26:25 +0700

    CommitAwal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  boardGame: number[][] = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+  ];
+  baris = 0;
+  kolom = 0;
+  myCounter = 0;
+
+  isKlik() {
+    // this.checkWin();
+    // if (this.myCounter % 2 == 0) {
+    //   this.boardGame[this.baris - 1][this.kolom - 1] = 1;
+    // } else {
+    //   this.boardGame[this.baris - 1][this.kolom - 1] = 2;
+    // }
+    this.myCounter += 1;
+    this.boardGame[2][3] = 24
+  }
+  checkWin() {}
 }
